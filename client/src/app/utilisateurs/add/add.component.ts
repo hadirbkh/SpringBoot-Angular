22942544b29a8ac100694f6b565496843ccdbab5
hadirbkh <hadir.benkhalfallah@etudiant-isi.utm.tn>
2025-04-13 00:31:44 +0100

    CRUD user DONE

--- client/src/app/utilisateurs/add/add.component.ts
+++ client/src/app/utilisateurs/add/add.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { Utilisateur } from 'src/app/models/utilisateur';
+import { UtilisateursService } from 'src/app/services/utilisateurs.service';
 
 @Component({
   selector: 'app-add',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./add.component.css']
 })
 export class AddComponent {
+  readonly dialogRef = inject(MatDialogRef<AddComponent>);  
+  hide = true;
+  utilisateur : Utilisateur={login:'',password:'',role:{id:3}}as Utilisateur;
+  constructor(private utilisateurService: UtilisateursService) {}
+  
+  clickEvent(event: MouseEvent) {
+    this.hide=!this.hide;
+    event.stopPropagation();
+  }
 
+  submitUser(){
+    this.utilisateurService.createUser(this.utilisateur).subscribe((data: Utilisateur) => {
+      this.utilisateur = data;
+      console.log(data);
+    });  }
+  
+  
 }
--- client/src/app/utilisateurs/utilisateurs.module.ts
+++ client/src/app/utilisateurs/utilisateurs.module.ts
@@ -8,8 +8,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-
-// Components
+import {MatInputModule} from '@angular/material/input';
 import { UtilisateursComponent } from './utilisateurs.component';
 import { ViewComponent } from './view/view.component';
 import { EditComponent } from './edit/edit.component';
@@ -17,6 +16,10 @@ import { DeleteComponent } from './delete/delete.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AddComponent } from './add/add.component';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -36,7 +39,11 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatCheckboxModule,
     MatIconModule,
     MatDividerModule,
-    MatProgressSpinnerModule,MatSnackBarModule
+    MatProgressSpinnerModule,MatSnackBarModule,
+    MatDialogModule,
+    MatButtonModule,MatFormFieldModule,
+    MatInputModule,FormsModule,
+    
   ],
   exports: [
     // Add any components you want to make available to other modules
